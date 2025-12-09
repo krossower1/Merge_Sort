bdeb6b14967233e95af272f76d50e1c4eada2ea3
krossower1 <121849382+krossower1@users.noreply.github.com>
2025-12-09 18:59:54 +0100

    Update main function for Google Test integration
    Modified main function to accept command line arguments and initialize Google Test framework.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 *
 **/
 
-int main() {
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    
     vector<int> a = { 4, 12, 67, 24, 547653, 19, 2, -5, -1, 0, 67, -1  };
     vector<double> b = { 4.1, 12.3, 67, 24.12, 54.999, 19.6, 2.2, -5.9, -1, 0, 67, -1 };
 
@@ -37,5 +39,7 @@ int main() {
     lista2.mergesort();
     lista2.print();
 
-    return 0;
+    cout << endl << endl;
+
+    return RUN_ALL_TESTS();
 }
